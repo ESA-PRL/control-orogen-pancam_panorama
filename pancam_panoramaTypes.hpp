4f7d5e6237ef4dfb10dd0bc1ea96580be1bc34f6
Karl Kangur <karl.kangur@gmail.com>
2016-11-11 10:18:29 +0100

    First commit

---
+++ pancam_panoramaTypes.hpp
@@ -0,0 +1,15 @@
+#ifndef pancam_panorama_TYPES_HPP
+#define pancam_panorama_TYPES_HPP
+
+/* If you need to define types specific to your oroGen components, define them
+ * here. Required headers must be included explicitly
+ *
+ * However, it is common that you will only import types from your library, in
+ * which case you do not need this file
+ */
+
+namespace pancam_panorama {
+}
+
+#endif
+
